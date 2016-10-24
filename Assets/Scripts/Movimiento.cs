3d857716e81f11d0030adda0803a8b1c1a985cdc
Nel <al228478@uji.es>
2016-10-24 13:23:25 +0200

    0.0.1
    - Creado un test para moverse en 2.5D

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimiento : MonoBehaviour {
+
+    private Rigidbody miRigidBody;
+
+	// Use this for initialization
+	void Start() {
+
+        miRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+
+        Debug.Log("Input: " + (Input.GetAxis("Horizontal")));
+
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            Vector3 movimiento = new Vector3(miRigidBody.position.x + 1, miRigidBody.position.y, miRigidBody.position.z);
+            miRigidBody.MovePosition(movimiento);
+
+        }else if(Input.GetAxis("Horizontal") < -0.1f)
+        {
+            Vector3 movimiento = new Vector3(miRigidBody.position.x - 1, miRigidBody.position.y, miRigidBody.position.z);
+            miRigidBody.MovePosition(movimiento);
+        }
+	}
+}
