557e07f03c623bdd170909708c4495905c81fb75
Nel <al228478@uji.es>
2016-10-25 12:46:36 +0200

    0.0.2
    - Arregladas las físicas del prota :tada:
    - Tenemos cámara!!!! :dancer:

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -5,8 +5,15 @@ public class Movimiento : MonoBehaviour {
 
     private Rigidbody miRigidBody;
 
-	// Use this for initialization
-	void Start() {
+    public float velocidad;
+    public float fuerzaSalto;
+
+    private bool enElAire;
+    public bool enElSuelo;
+    private bool saltoListo;
+
+    // Use this for initialization
+    void Start() {
 
         miRigidBody = GetComponent<Rigidbody>();
 	}
@@ -14,8 +21,14 @@ public class Movimiento : MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
 
-        Debug.Log("Input: " + (Input.GetAxis("Horizontal")));
+        if (Input.GetAxis("Horizontal") != 0f)
+        {
+            Vector3 movimiento = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+            movimiento *= velocidad;
+            transform.Translate(movimiento * Time.deltaTime);
+        }
 
+        /*
         if (Input.GetAxis("Horizontal") > 0.1f)
         {
             Vector3 movimiento = new Vector3(miRigidBody.position.x + 1, miRigidBody.position.y, miRigidBody.position.z);
@@ -26,5 +39,30 @@ public class Movimiento : MonoBehaviour {
             Vector3 movimiento = new Vector3(miRigidBody.position.x - 1, miRigidBody.position.y, miRigidBody.position.z);
             miRigidBody.MovePosition(movimiento);
         }
+        */
 	}
+
+    void FixedUpdate()
+    {
+        if (enElSuelo && Input.GetKeyDown("space"))
+        {
+            miRigidBody.AddForce(new Vector3(0, fuerzaSalto, 0));
+        }
+    }
+
+    void OnTriggerEnter(Collider otro)
+    {
+        if (otro.tag == "suelo")
+        {
+            enElSuelo = true;
+        }
+    }
+
+    void OnTriggerExit(Collider otro)
+    {
+        if (otro.tag == "suelo")
+        {
+            enElSuelo = false;
+        }
+    }
 }
---
+++ Assets/Scripts/SeguimientoCamara.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeguimientoCamara : MonoBehaviour {
+
+    public float dampTime = 0.15f;
+    private Vector3 velocity = Vector3.zero;
+    public Transform target;
+    Camera camera;
+    GameObject player;
+    Movimiento movimientoPlayer;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        movimientoPlayer = player.GetComponent<Movimiento>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            if (movimientoPlayer.enElSuelo)
+            {
+                Vector3 point = camera.WorldToViewportPoint(target.position);
+                Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+                Vector3 destination = transform.position + delta;
+                transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+            }else
+            {
+                Vector3 point = camera.WorldToViewportPoint(target.position);
+                Vector3 delta = new Vector3(target.position.x, transform.position.y, target.position.z) - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+                Vector3 destination = transform.position + delta;
+                transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+            }
+        }
+
+    }
+}
