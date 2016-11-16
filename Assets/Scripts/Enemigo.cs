c2a71ebf030bad449a9421b8ab730ac86473a9a1
ushiroM <carlosgalibar@gmail.com>
2016-11-16 18:20:39 +0100

    0.0.5
    hay un enemigo y se muere al saltarle encima, pero el movimiento falta
    terminarlo

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemigo : MonoBehaviour {
+
+    // Use this for initialization
+   
+    Vector3 movimiento;
+    float distanciaMax = 1;
+    float distanciaInicial;
+    float distancia = 0;
+    
+    void Awake()
+    {
+        distanciaInicial = transform.position.x;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Move();
+    }
+    public void Muerto()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Move()
+    {
+        distancia = transform.position.x - distanciaInicial;
+        if (distancia <= distanciaMax) {
+            movimiento = new Vector3(transform.position.x - 0.1f, transform.position.y, transform.position.z);
+
+        } else if (distancia >= distanciaMax) {
+            movimiento = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
+        }
+             
+        transform.position = movimiento;
+    }
+   
+}
+
---
+++ Assets/Scripts/EnemigoMuerto.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemigoMuerto : MonoBehaviour {
+
+    Enemigo enemy;
+    
+    void Start()
+    {
+        enemy = GetComponentInParent<Enemigo>();
+    }
+	// Use this for initialization
+	void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            enemy.Muerto();
+        }
+    }
+}
