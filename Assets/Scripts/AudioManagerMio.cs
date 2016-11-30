1c52e412294a7efbb71b479532370bce20250007
mayoscuro <jesumayoral@hotmail.com>
2016-11-30 22:52:13 +0100

    0.1.5
    -Mis errores han sido perdonados volviendo al pasado.
    -Las musiquillas ya funcionan(ajustar valores de volumen si es que hace
    falta).
    -Se gana el juego.
    -Y creo que ya esta, pero igual en otra linea temporal hice más cosas.

---
+++ Assets/Scripts/AudioManagerMio.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AudioManagerMio : MonoBehaviour {
+    public AudioSource violin;
+    public AudioSource viola;
+    public AudioSource violonchelo;
+    public AudioSource fagot;
+    public AudioSource flauta;
+    public AudioSource oboe;
+    public AudioSource basso;
+    public AudioSource clarinete;
+
+    public static bool esviolin = false;
+    public static bool esviola = false;
+    public static bool esviolonchelo = false;
+    public static bool esfagot = false;
+    public static bool esflauta = false;
+    public static bool esoboe = false;
+    public static bool esbasso = false;
+    public static bool esclarinete = false;
+
+
+    private GameObject victoria;
+
+    // Use this for initialization
+    void Start () {
+        victoria = GameObject.FindGameObjectWithTag("Victoria");
+        victoria.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(esbasso+ "," + esclarinete + "," + esfagot + "," + esflauta + "," + esoboe + "," + esviola + "," + esviolin + "," + esviolonchelo);
+        if (esviolin) {
+            violin.mute = false;
+        }
+        if (esviola)
+        {
+            viola.mute = false;
+        }
+        if (esviolonchelo)
+        {
+            violonchelo.mute = false;
+        }
+        if (esfagot)
+        {
+            violonchelo.mute = false;
+        }
+        if (esoboe)
+        {
+            oboe.mute = false;
+        }
+        if (esflauta)
+        {
+            flauta.mute = false;
+        }
+        if (esbasso)
+        {
+            basso.mute = false;
+        }
+        if (esclarinete)
+        {
+            clarinete.mute = false;
+        }
+
+        //El if del vencedor xD
+        if (esbasso && esclarinete && esfagot && esflauta && esoboe && esviola && esviolin && esviolonchelo) {
+            victoria.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/Instrumentos.cs
+++ Assets/Scripts/Instrumentos.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class Instrumentos : MonoBehaviour {
-
+    //private AudioManagerMio audioManager;
 	// Use this for initialization
 	void Start () {
-	
+      //  audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManagerMio>();
 	}
 	
 	// Update is called once per frame
@@ -15,6 +15,40 @@ public class Instrumentos : MonoBehaviour {
 
     void OnTriggerEnter(Collider collider) {
         if (collider.tag == "Player") {
+            if (gameObject.tag == "Violines") {
+                AudioManagerMio.esviolin = true;
+            } else if (gameObject.tag == "Violines") {
+                AudioManagerMio.esviolin = true;
+            }
+            else if (gameObject.tag == "Viola")
+            {
+                AudioManagerMio.esviola = true;
+            }
+            else if (gameObject.tag == "Violonchelo")
+            {
+                AudioManagerMio.esviolonchelo = true;
+            }
+            else if (gameObject.tag == "Fagot")
+            {
+                AudioManagerMio.esfagot = true;
+            }
+            else if (gameObject.tag == "Oboe")
+            {
+                AudioManagerMio.esoboe = true;
+            }
+            else if (gameObject.tag == "Flauta")
+            {
+                AudioManagerMio.esflauta = true;
+            }
+            else if (gameObject.tag == "Basso")
+            {
+                AudioManagerMio.esbasso = true;
+            }
+            else if (gameObject.tag == "Clarinete")
+            {
+                AudioManagerMio.esclarinete = true;
+            }
+
             Destroy(gameObject);
         }
     }
