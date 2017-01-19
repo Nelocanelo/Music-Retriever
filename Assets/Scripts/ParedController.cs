71281d8bb5cb2436bd272ea8221462b101f6d989
Karrion <al187874@uji.es>
2017-01-19 19:52:36 +0100

    0.1.6
    -Añadido Nivel 2
    -Añadida música
    -Cabezazos dados contra la pared

---
+++ Assets/Scripts/ParedController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParedController : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+        if (Input.GetAxis("Vertical"))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 5 * Time.deltaTime,transform.position.z);
+        }
+	}
+}
