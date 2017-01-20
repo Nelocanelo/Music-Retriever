07b74d331d7d88642a5a37d57d42d5d658d53508
mayoscuro <jesumayoral@hotmail.com>
2017-01-21 00:12:43 +0100

    0.2.4
    El enemigo va como toca en el nivel 1 (el tutorial de Nel), no os lo
    cargueís que aun no tiene prefab que le faltan 4 cosas

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -7,11 +7,13 @@ public class Enemigo : MonoBehaviour {
     public float velocidad;
     MovimientoCharacterController playerScript;
     public bool pisado = false;
+    private Animator animador;
 
     void Awake()
     {
         x0 = transform.position.x;
         playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<MovimientoCharacterController>();
+        animador = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -26,7 +28,19 @@ public class Enemigo : MonoBehaviour {
 
     private void Move()
     {
+        if (velocidad > 0)
+        {
+            animador.SetBool("Correr", true);
+        }
+        else {
+            animador.SetBool("Correr", false);
+        }
         gameObject.transform.position = new Vector3(x0 + 3f * Mathf.Sin(velocidad * Time.time), gameObject.transform.position.y, gameObject.transform.position.z);
+        if ((x0 + 3f * Mathf.Sin(velocidad * Time.time))  < x0 - 2.99f){
+            transform.rotation = Quaternion.AngleAxis(104, Vector3.up);
+        } else if ((x0 + 3f * Mathf.Sin(velocidad * Time.time)) >= x0 + 2.99f) {
+            transform.rotation = Quaternion.AngleAxis(280, Vector3.up);
+        }
     }
     
     void OnTriggerEnter(Collider otro)
