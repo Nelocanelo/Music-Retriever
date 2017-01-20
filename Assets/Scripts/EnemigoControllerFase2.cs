2b1f6d20384f47265d1613947132282eec97bfaa
Oscar Gil Ferrer <al187874@uji.es>
2017-01-20 21:50:56 +0100

    0.2.3
    -La pared se mueve
    -Ganas puntos al bloquear enemigos
    -El mecanismo de spawneo funciona excepto por el tiempo

---
+++ Assets/Scripts/EnemigoControllerFase2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoControllerFase2 : MonoBehaviour {
+
+    GameControllerFase2 controller;
+    float speed;
+
+
+	// Use this for initialization
+	void Start () {
+        controller = GameObject.FindGameObjectWithTag("GameControllerFase2").GetComponent<GameControllerFase2>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        speed = controller.speed;
+        transform.position = new Vector3(transform.position.x - (speed * Time.deltaTime), transform.position.y, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/GameControllerFase2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerFase2 : MonoBehaviour {
+
+    public float life;
+    public int score;
+    GameObject[] spawners;
+    public GameObject prefab;
+    public float speed = 0.5f;
+
+	void Start () {
+        spawners = GameObject.FindGameObjectsWithTag("Spawner");
+	}
+	
+	
+	void Update () {
+        Debug.Log(Time.time);
+		if(Time.time%5 == 0)
+        {
+            int random = Mathf.Abs(Random.Range(0, 3));
+            Instantiate(prefab, spawners[random].transform.position,Quaternion.Euler(0.0f,0.0f,0.0f));
+        }
+	}
+}
