560df24ae43d2ede7908798a3d44a5712f3cac30
mayoscuro <jesumayoral@hotmail.com>
2017-01-22 17:21:11 +0100

    0.3.0
    -Animación del Menu terminada
    -Botones del menu en proceso
    -Scripts y cosas para que vaya el Menu
    -y prefebs especiales para el menu
    -Y creo que nada más.

---
+++ Assets/Scripts/Menu/AnimacionesDelMenu.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimacionesDelMenu : MonoBehaviour {
+    private GameObject enemigo;
+    private Animator animadorEnemigo;
+    private GameObject perro;
+    private Animator animadorPerro;
+    private GameObject movedorMenu;
+    private GameObject particulas;
+
+
+    //Booleanos que controlan la animación:
+    private bool encontrarMusica = true;//Enemigo camina hacia las notas
+    private bool coger;//El enemigo se rie al ver las notas
+    private bool ladrido;//El perro ladra desde fuera de plano
+    private bool perroCorriendo;//Perro persigue al enemigo
+    private bool enemigoCorriendo;//Perro persigue al enemigo
+    private bool menuBajando;//Baja el menu mientras el perro persigue al enemigo y desaparecen
+    private bool destruirlos;//Porque han salido del plano de la camara.
+    private bool pararChiringuito;
+	
+	void Start () {
+        movedorMenu = GameObject.FindGameObjectWithTag("MovedorMenu");
+        enemigo = GameObject.FindGameObjectWithTag("Enemy");
+        perro = GameObject.FindGameObjectWithTag("Player");
+        particulas = GameObject.FindGameObjectWithTag("Particulas");
+        animadorEnemigo = enemigo.GetComponent<Animator>();
+        animadorPerro = perro.GetComponent<Animator>();
+    }
+
+    IEnumerator encontrarMusicaf() {
+        animadorEnemigo.SetBool("Correr",true);
+        enemigo.transform.position = new Vector3(enemigo.transform.position.x+0.04f, enemigo.transform.position.y, enemigo.transform.position.z);
+        yield return new WaitForSeconds(5f);
+        StopCoroutine("encontrarMusicaf");
+        encontrarMusica = false;
+        animadorEnemigo.SetBool("Correr", false);
+        coger = true;
+    }
+
+    IEnumerator cogerf()
+    {
+        animadorEnemigo.SetBool("Coger", true);
+        yield return new WaitForSeconds(0.5f);
+        particulas.SetActive(false);
+        StopCoroutine("cogerf");
+        coger = false;
+        animadorEnemigo.SetBool("Coger", false);
+        ladrido = true;
+    }
+
+    IEnumerator ladridof() {
+        //ladrido (sonido)
+        yield return new WaitForSeconds(1f);
+        StopCoroutine("ladridof");
+        ladrido = false;
+        perroCorriendo = true;
+    }
+
+    IEnumerator perroCorriendof()
+    {
+        animadorPerro.SetBool("Correr", true);
+        perro.transform.position = new Vector3(perro.transform.position.x + 0.05f, perro.transform.position.y, perro.transform.position.z);
+        yield return new WaitForSeconds(4f);
+        animadorPerro.SetBool("Correr", true);
+        perro.transform.position = new Vector3(perro.transform.position.x + 0.03f, perro.transform.position.y, perro.transform.position.z);
+        enemigoCorriendo = true;
+    }
+
+    IEnumerator enemigoCorriendof() {
+        animadorEnemigo.SetBool("Correr", true);
+        enemigo.transform.position = new Vector3(enemigo.transform.position.x + 0.04f, enemigo.transform.position.y, enemigo.transform.position.z);
+        yield return new WaitForSeconds(1);
+        menuBajando = true;
+        animadorEnemigo.SetBool("Correr", true);
+        enemigo.transform.position = new Vector3(enemigo.transform.position.x + 0.04f, enemigo.transform.position.y, enemigo.transform.position.z);
+
+    }
+
+    void bajarMenu() {
+        // movedorMenu.GetComponent<RectTransform>().localPosition = new Vector3(movedorMenu.transform.position.x, perro.transform.position.y - 0.001f, perro.transform.position.z); ;
+        if (movedorMenu.transform.localPosition.y >= 0)
+        {
+            movedorMenu.transform.position = new Vector3(movedorMenu.transform.position.x, movedorMenu.transform.position.y - 1f, movedorMenu.transform.position.z);
+        }
+        else
+        {
+            StopAllCoroutines();
+            menuBajando = false;
+            destruirlos = true;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) {
+            pararChiringuito = true;
+        }
+
+        if (encontrarMusica && ! pararChiringuito)
+        {
+            StartCoroutine("encontrarMusicaf");
+        }
+        else if (coger && !pararChiringuito) {
+            StartCoroutine("cogerf");
+        } else if (ladrido && !pararChiringuito) {
+            StartCoroutine("ladridof");
+        } else if (perroCorriendo && !pararChiringuito) {
+            StartCoroutine("perroCorriendof");
+        }
+
+        if (enemigoCorriendo && !pararChiringuito) {
+            StartCoroutine("enemigoCorriendof");
+        }
+
+        if (menuBajando && !pararChiringuito) {
+            bajarMenu();
+        }
+
+        if (destruirlos && !pararChiringuito) {
+            Destroy(perro);
+            Destroy(enemigo);
+        }
+
+        if (pararChiringuito) {
+            Destroy(perro);
+            Destroy(enemigo);
+            Destroy(particulas);
+            StopAllCoroutines();
+            movedorMenu.transform.localPosition = new Vector3(movedorMenu.transform.localPosition.x, 0, movedorMenu.transform.localPosition.z);
+        }
+	}
+}
---
+++ Assets/Scripts/Menu/ClickMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ClickMenu : MonoBehaviour {
+    
+    public void empezarTutorial() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void empezarModo1() {
+        SceneManager.LoadScene(2);
+    }
+
+    public void empezarModo2() {
+
+    }
+
+    public void salir() {
+        Application.Quit();
+    }
+}
