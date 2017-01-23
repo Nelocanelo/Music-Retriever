8f3c73afa575ec7fdde33162263e2d4bcde736ec
Karrion <al187874@uji.es>
2017-01-23 16:55:51 +0100

    0.3.4
    La Fase 2 es ganable :dancer:

--- Assets/Scripts/EnemigoControllerFase2.cs
+++ Assets/Scripts/EnemigoControllerFase2.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 public class EnemigoControllerFase2 : MonoBehaviour {
 
     GameControllerFase2 controller;
-    public float speed = 0.5f;
+    public int tipo;
+    public float speed;
 
 
 	// Use this for initialization
 	void Start () {
         controller = GameObject.FindGameObjectWithTag("GameControllerFase2").GetComponent<GameControllerFase2>();
+        switch (tipo)
+        {
+            case 1:
+                speed = 1f;
+                break;
+            case 2:
+                speed = 0.9f;
+                break;
+            case 3:
+                speed = 0.7f;
+                break;
+            case 4:
+                speed = 0.3f;
+                break;
+        }
 	}
 	
 	// Update is called once per frame
