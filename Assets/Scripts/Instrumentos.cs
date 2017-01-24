a3283e3eae9c4231cec1a6e4ee7cba4fe3219c3f
mayoscuro <jesumayoral@hotmail.com>
2017-01-24 19:17:56 +0100

    0.4.2
    Instrumentos aleatorios conseguido;
    Peta por cosas de bucles, pero tranquilos ^.^

--- Assets/Scripts/Instrumentos.cs
+++ Assets/Scripts/Instrumentos.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class Instrumentos : MonoBehaviour {
     //private AudioManagerMio audioManager;
-	// Use this for initialization
+    // Use this for initialization
+    private AudioManagerMio instrumentos;
 	void Start () {
-      //  audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManagerMio>();
-	}
+        //  audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManagerMio>();
+        instrumentos = GameObject.FindObjectOfType<AudioManagerMio>().GetComponent<AudioManagerMio>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -14,42 +16,51 @@ public class Instrumentos : MonoBehaviour {
 	}
 
     void OnTriggerEnter(Collider collider) {
-        if (collider.tag == "Player") {
+        if (collider.tag == "Player" && instrumentos.instrumentos[instrumentos.objetivo].tag == gameObject.tag) {
             if (gameObject.tag == "Violines") {
                 AudioManagerMio.esviolin = true;
+                Destroy(gameObject);
             } else if (gameObject.tag == "Violines") {
                 AudioManagerMio.esviolin = true;
+                Destroy(gameObject);
             }
             else if (gameObject.tag == "Viola")
             {
                 AudioManagerMio.esviola = true;
+                Destroy(gameObject);
             }
             else if (gameObject.tag == "Violonchelo")
             {
                 AudioManagerMio.esviolonchelo = true;
+                Destroy(gameObject);
             }
             else if (gameObject.tag == "Fagot")
             {
                 AudioManagerMio.esfagot = true;
+                Destroy(gameObject);
             }
             else if (gameObject.tag == "Oboe")
             {
                 AudioManagerMio.esoboe = true;
+                Destroy(gameObject);
             }
             else if (gameObject.tag == "Flauta")
             {
                 AudioManagerMio.esflauta = true;
+                Destroy(gameObject);
             }
             else if (gameObject.tag == "Basso")
             {
                 AudioManagerMio.esbasso = true;
+                Destroy(gameObject);
             }
             else if (gameObject.tag == "Clarinete")
             {
                 AudioManagerMio.esclarinete = true;
+                Destroy(gameObject);
             }
 
-            Destroy(gameObject);
+            
         }
     }
 }
