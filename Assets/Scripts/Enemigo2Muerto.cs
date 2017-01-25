6c98288d1f4e06e0ea7d87a65929ad9db4c37cc3
mayoscuro <jesumayoral@hotmail.com>
2017-01-25 22:24:41 +0100

    0.5.5
    Puntos al derrotar enemigos son mostrados ;P

--- Assets/Scripts/Enemigo2Muerto.cs
+++ Assets/Scripts/Enemigo2Muerto.cs
@@ -9,12 +9,15 @@ public class Enemigo2Muerto : MonoBehaviour
     int puntos = 10;
     Collider colisionador;
     Rigidbody colliderPadre;
+    GameObject puntosFlotantes;
 
     void Start()
     {
         enemy = GetComponentInParent<Enemigo2>();
         colisionador = GetComponent<Collider>();
         colliderPadre = GetComponentInParent<Rigidbody>();
+        puntosFlotantes = gameObject.transform.GetChild(0).gameObject;
+        puntosFlotantes.SetActive(false);
     }
     // Use this for initialization
     void OnTriggerEnter(Collider other)
@@ -28,8 +31,24 @@ public class Enemigo2Muerto : MonoBehaviour
             colisionador.enabled = false;
             colliderPadre.detectCollisions = false;
             colliderPadre.useGravity = false;
+            puntosFlotantes.SetActive(true);
             enemy.gameObject.transform.position = new Vector3(enemy.gameObject.transform.position.x, enemy.gameObject.transform.position.y, enemy.gameObject.transform.position.z + 1.5f);
             // enemy.Muerto();
+        }
+    }
+
+    IEnumerator movimientoPuntos()
+    {
+        puntosFlotantes.transform.position = new Vector3(puntosFlotantes.transform.position.x, puntosFlotantes.transform.position.y + 0.015f, puntosFlotantes.transform.position.z);
+        yield return new WaitForSeconds(2.5f);
+        puntosFlotantes.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (enemy.pisado)
+        {
+            StartCoroutine("movimientoPuntos");
         }
     }
 }
--- Assets/Scripts/EnemigoMuerto.cs
+++ Assets/Scripts/EnemigoMuerto.cs
@@ -6,6 +6,7 @@ public class EnemigoMuerto : MonoBehaviour {
     Enemigo enemy;
     Collider colisionador;
     Rigidbody colliderPadre;
+    GameObject puntosFlotantes;
     int puntos = 10;
     
     void Start()
@@ -13,6 +14,8 @@ public class EnemigoMuerto : MonoBehaviour {
         enemy = GetComponentInParent<Enemigo>();
         colisionador = GetComponent<Collider>();
         colliderPadre = GetComponentInParent<Rigidbody>();
+        puntosFlotantes = gameObject.transform.GetChild(0).gameObject;
+        puntosFlotantes.SetActive(false);
     }
 	// Use this for initialization
 	void OnTriggerEnter(Collider other)
@@ -25,8 +28,23 @@ public class EnemigoMuerto : MonoBehaviour {
             colisionador.enabled = false;
             colliderPadre.detectCollisions = false;
             colliderPadre.useGravity = false;
+            puntosFlotantes.SetActive(true);
             enemy.gameObject.transform.position = new Vector3(enemy.gameObject.transform.position.x, enemy.gameObject.transform.position.y, enemy.gameObject.transform.position.z +1.5f);
             //enemy.Muerto();
+        }
+    }
+
+    IEnumerator movimientoPuntos() {
+        puntosFlotantes.transform.position = new Vector3(puntosFlotantes.transform.position.x, puntosFlotantes.transform.position.y + 0.015f, puntosFlotantes.transform.position.z);
+        yield return new WaitForSeconds(2.5f);
+        puntosFlotantes.SetActive(false);
+    }
+
+    void Update() {
+        puntosFlotantes.transform.rotation = Quaternion.AngleAxis(-9.256001f, Vector3.up);
+        if (enemy.pisado)
+        {
+            StartCoroutine("movimientoPuntos");
         }
     }
 }
